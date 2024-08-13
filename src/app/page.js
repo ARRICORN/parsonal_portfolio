409f8d77028787b57ad85362e3fdc76e06bb2e77
Shormily <shormilyrais@gmail.com>
2024-08-13 19:13:02 +0600

    complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import About from "./component/about";
+import Expericnce from "./component/Expericnce";
+import Contact from "./component/Contact/Contact";
 
 export default function Home() {
   return (
-    <> <p>HI SWEETY</p></>
+    <div className='bg-[#1E1E1E]'>
+      <About />
+      <Expericnce/>
+      <Contact/>
+    </div>
   );
 }
